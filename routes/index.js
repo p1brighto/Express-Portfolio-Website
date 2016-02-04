d4845f6d715f0fad3e69b74608dba9a8a3b0e5e9
p1brighto <p1brighto@gmail.com>
2016-02-03 22:29:56 -0500

    test again

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-/* GET home page. */
+/* GET home page. *
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
+});*/
+router.get('/', function (req, res, next) {
+    //res.send('Hello Express!');
+    res.sendFile(path.join(__dirname, "views/test", "index.html"));
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) {
