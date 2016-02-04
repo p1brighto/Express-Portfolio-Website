26c5ecaac5fd585c29f49170338e1f9c65514108
p1brighto <p1brighto@gmail.com>
2016-02-03 20:17:27 -0500

    added the pages

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+/* GET service page. */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Service' });
+});
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Project' });
+});
+/* GET cotact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
 
 module.exports = router;
